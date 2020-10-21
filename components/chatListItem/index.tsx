5d4d8c725e5fe60fe1fc35ed7a736d55b1a768ed
Suraj Thapa <stsurajst@gmail.com>
2020-10-21 19:19:59 +0545

    initial chat list component done

--- components/chatListItem/index.tsx
+++ components/chatListItem/index.tsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import { View, Text, Image } from 'react-native'
-import { ChatRoom } from '../../types'
+import { View, Text, Image } from 'react-native';
+import { ChatRoom } from '../../types';
 
-import styles from './style'
+import styles from './style';
 
 export type ChatListItemProps = {
-    chatRoom: ChatRoom
-}
+  chatRoom: ChatRoom;
+};
 
-const ChatListItem = (props: ChatListItemProps) =>{
-    const {chatRoom} = props
-     console.log("user>>",chatRoom)
-    const user = chatRoom.users[0];
-    
-   
-    return(
-    <View>
-        <Image source={{ uri: user.imageUri}} style= {styles.avatar}/>
-        <Text>{chatRoom.lastMessage.content}</Text>
+const ChatListItem = (props: ChatListItemProps) => {
+  const { chatRoom } = props;
+  console.log('user>>', chatRoom);
+  const user = chatRoom.users[0];
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.leftContainer}>
+        <Image source={{ uri: user.imageUri }} style={styles.avatar} />
+        <View style={styles.midContainer}>
+          <Text style={styles.username}>{user.name}</Text>
+          <Text style={styles.lastMessage}>{chatRoom.lastMessage.content}</Text>
+        </View>
+      </View>
+      {/* <Text>{chatRoom.lastMessage.createdAt}</Text> */}
+      <Text style={styles.time}>Yesterday</Text>
     </View>
-        )
-
-}
+  );
+};
 
 export default ChatListItem;
