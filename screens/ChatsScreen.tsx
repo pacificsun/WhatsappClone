3d5a89b326fcd3ffad2d833490907750755edd01
Suraj Thapa <stsurajst@gmail.com>
2020-10-24 22:43:41 +0545

    import adjusted

--- screens/ChatsScreen.tsx
+++ screens/ChatsScreen.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import { StyleSheet, FlatList } from 'react-native';
-
-import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
 import ChatListItem from '../components/chatListItem';
