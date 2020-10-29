b0115cce812f9fd58758e291c092370d0e8d50f1
Suraj Thapa <stsurajst@gmail.com>
2020-10-29 23:13:11 +0545

    style for chat message

--- components/ChatMessage/index.tsx
+++ components/ChatMessage/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 import moment from 'moment';
 
 import { Message } from '../../types';
-
+import styles from './style';
 export type ChatMessageProps = {
   message: Message;
 };
@@ -12,10 +12,12 @@ export type ChatMessageProps = {
 const ChatMessage = (props: ChatMessageProps) => {
   const { message } = props;
   return (
-    <View>
-      <Text>{message.user.id}</Text>
-      <Text>{message.content}</Text>
-      <Text>{moment(message.createdAt).fromNow()}</Text>
+    <View style={styles.container}>
+      <View style={styles.messageBox}>
+        <Text>{message.user.id}</Text>
+        <Text>{message.content}</Text>
+        <Text>{moment(message.createdAt).fromNow()}</Text>
+      </View>
     </View>
   );
 };
