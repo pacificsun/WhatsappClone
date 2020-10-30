02c1949f7215d8d153ecfee068019d2d532ca2af
Suraj Thapa <stsurajst@gmail.com>
2020-10-30 15:37:14 +0545

    backgaround image added for chat message and flat list inverted to get latest date at the buttom

--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, ImageBackground } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 import { FlatList } from 'react-native-gesture-handler';
 
 import chatRoomData from '../data/Chats';
 import ChatMessage from '../components/ChatMessage';
+import BG from '../assets/images/BG.png';
 const ChatRoomScreen = () => {
   const route = useRoute();
   // console.log('route>>', route.params);
   return (
-    <FlatList
-      data={chatRoomData.messages}
-      renderItem={({ item }) => <ChatMessage message={item} />}
-    />
+    <ImageBackground style={{ width: '100%', height: '100%' }} source={BG}>
+      <FlatList
+        data={chatRoomData.messages}
+        renderItem={({ item }) => <ChatMessage message={item} />}
+        inverted
+      />
+    </ImageBackground>
   );
 };
 
