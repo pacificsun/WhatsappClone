8f9f0ca57f95831ff49e335420be117af8a5068a
Suraj Thapa <stsurajst@gmail.com>
2020-10-30 23:54:45 +0545

    style and component done for message box

--- components/InputBox/index.tsx
+++ components/InputBox/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, TextInput } from 'react-native';
+import {
+  MaterialCommunityIcons,
+  FontAwesome5,
+  Fontisto,
+  Entypo,
+} from '@expo/vector-icons';
+import styles from './styles';
+
 const InputBox = () => {
   return (
-    <View>
-      <Text>Hello There</Text>
+    <View style={styles.container}>
+      <View style={styles.mainContainer}>
+        <FontAwesome5 name="laugh-beam" size={24} color="grey" />
+        <TextInput style={styles.textInput} multiline />
+        <Entypo style={styles.icon} name="attachment" size={24} color="grey" />
+        <Fontisto style={styles.icon} name="camera" size={24} color="grey" />
+      </View>
+      <View style={styles.buttonContainer}>
+        <MaterialCommunityIcons name="microphone" size={28} color="white" />
+      </View>
     </View>
   );
 };
