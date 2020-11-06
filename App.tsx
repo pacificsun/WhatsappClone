4f8c29a7ea4e85501dc63ecbf4aa6aac8cd60502
Suraj Thapa <stsurajst@gmail.com>
2020-11-06 14:05:10 +0545

    auth is used in app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+import { Auth } from 'aws-amplify';
 
 import { withAuthenticator } from 'aws-amplify-react-native';
 
@@ -20,6 +22,23 @@ Amplify.configure({
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+
+  // Run this snippet only when app is first mounted
+  useEffect(() => {
+    const fetchUser = async () => {
+      // Get authenticated user from auth
+      const userInfo = await Auth.currentAuthenticatedUser({
+        bypassCache: true,
+      }); // bypassCache means we are not taking from cache but from server
+
+      console.log('userInfo>>', userInfo);
+      if (userInfo) {
+        // Get the user from Backend with user sub from Auth
+        // if there is no user in db with the id then create one
+      }
+    };
+    fetchUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
